b96a050ea1edc06e4349931cacbf7d32c2bece2e
Keith Benjamin <kjb528i@yahoo.com>
2022-04-14 17:42:30 -0400

    Added to UserController

--- src/main/java/com/careerdevs/gorestsql/controllers/UserController.java
+++ src/main/java/com/careerdevs/gorestsql/controllers/UserController.java
@@ -1,4 +1,46 @@
 package com.careerdevs.gorestsql.controllers;
 // all code in here
+
+import com.careerdevs.gorestsql.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@RequestMapping("/user")
+
 public class UserController {
+
+    @GetMapping("/upload/{id}")
+    public ResponseEntity<?> uploadUserById(@PathVariable("id") String userId, RestTemplate restTemplate
+    ) {
+        try {
+
+            int uID = Integer.parseInt(userId);
+
+            //check the range => other things to do
+
+            String url = "https://gorest.co.in/public/v2/users/" + uID;
+
+            User foundUser = restTemplate.getForObject(url, User.class);
+
+            return new ResponseEntity<>("Temp", HttpStatus.OK);
+
+        } catch (NumberFormatException e) {
+
+            return new ResponseEntity<>("Id must be a number", HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
 }
