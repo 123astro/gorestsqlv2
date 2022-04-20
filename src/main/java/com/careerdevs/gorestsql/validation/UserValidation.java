d5c35794aa3c75f40b06aaaa97f11926244ed642
Keith Benjamin <kjb528i@yahoo.com>
2022-04-20 09:17:06 -0400

    Added errors checking

--- src/main/java/com/careerdevs/gorestsql/validation/UserValidation.java
+++ src/main/java/com/careerdevs/gorestsql/validation/UserValidation.java
@@ -28,6 +28,11 @@ public class UserValidation {
             }
         }
 
+        String userName = user.getName();
+        String userEmail = user.getEmail();
+        String userGender = user.getGender();
+        String userStatus = user.getStatus();
+
         if (user.getName() == null || user.getName().trim().equals("")) {
             errors.addError("name", "name can't be left blank");
         }
@@ -38,11 +43,16 @@ public class UserValidation {
 
         if (user.getGender() == null || user.getGender().trim().equals("")) {
             errors.addError("Gender", "Gender can't be left blank");
+        } else if (!(userGender.equals("male") || userGender.equals("female") || userGender.equals("other"))) {
+            errors.addError("gender", "Gender must be: male, female, other");
         }
 
         if (user.getStatus() == null || user.getStatus().trim().equals("")) {
             errors.addError("status", "Status can't be left blank");
+        } else if (!(userStatus.equals("active") || userStatus.equals("inactive"))) {
+            errors.addError("status", "Status must be: active or inactive");
         }
+
         return errors;
     }
 }
